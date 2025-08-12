2f4c4170ae53edb8a573ff50a7d319b9579f8e56
Vivek <vivek@medianv.com>
2025-08-12 16:53:11 +0530

    Add notification with email send functionality

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,5 +1,6 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable, Logger } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
+import { mailTransporter } from 'src/mail/sendMail.mail';
 
 @Injectable()
 export class NotificationService {
@@ -7,8 +8,19 @@ export class NotificationService {
     private readonly userService: UserService
     ) { }
 
-    sendNotification(userId: number, message: string) {
-        const user = this.userService.getUserById(userId)
-        console.log(`Send notification to ${user.name} : ${message}`)
+    //send notification using nodemailer;
+    async sendNotification(userId: number, message: string) {
+
+        const user = this.userService.getUserById(userId);
+
+        await mailTransporter.sendMail({
+            from: '"My App" <no-reply@myapp.com>',
+            to: user.email || 'vivek@test.com',
+            subject: 'Notification',
+            text: message,
+        });
+
+        Logger.log(`Send notification to ${user.name} : ${message}`)
+
     }
 }
