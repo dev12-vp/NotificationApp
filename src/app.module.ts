b0644513b52f704450074ed451a73c332c7efc43
Vivek <vivek@medianv.com>
2025-08-12 13:02:05 +0530

    Set up new notification project

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { NotificationModule } from './notification/notification.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [NotificationModule, UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
