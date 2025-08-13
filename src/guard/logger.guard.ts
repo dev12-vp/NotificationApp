24e988b55f600b7ec84191acf7330c4d122e73a6
Vivek <vivek@medianv.com>
2025-08-13 14:07:18 +0530

    Add note

--- src/guard/logger.guard.ts
+++ src/guard/logger.guard.ts
@@ -1,6 +1,14 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { Observable } from "rxjs";
 
+/* 
+ExecutionContext
+ A special object in NestJS lifecycle hooks (guards, interceptors, filters) that lets you:
+  🔹Access request details (method, URL, headers, params)
+  🔹Access user context (from authentication middleware/guards)
+  🔹Switch between HTTP, WebSocket, or gRPC contexts.
+*/
+
 @Injectable()
 export class LoggerGuard implements CanActivate {
     canActivate(context: ExecutionContext): boolean {
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,6 +2,13 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { LoggerGuard } from 'src/guard/logger.guard';
 
+/*
+Lazy-Loading in NestJS
+  🔹Means the module is only loaded when its route is first accessed (instead of at app startup).
+  🔹Helps with startup performance in large apps or when some features are rarely used.
+*/
+
+
 @Controller()
 export class ReportsController {
     constructor(private reportService: ReportsService) { }
