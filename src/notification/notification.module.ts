319ee3583603f520d588c01465582a2289c51f12
Vivek <vivek@medianv.com>
2025-08-13 17:17:04 +0530

    Add NestJS [200~ads via Carbon

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -2,9 +2,10 @@ import { forwardRef, Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
 import { UserModule } from 'src/user/user.module';
+import { MailModule } from 'src/mail/mail.module';
 
 @Module({
-  imports: [forwardRef(() => UserModule)],
+  imports: [forwardRef(() => UserModule),MailModule],
   providers: [NotificationService],
   controllers: [NotificationController],
   exports: [NotificationService]
--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,11 +1,13 @@
 import { forwardRef, Inject, Injectable, Logger } from '@nestjs/common';
+import { MailProvider } from 'src/mail/mail.provider';
 import { UserService } from 'src/user/user.service';
-import { mailTransporter } from 'src/mail/sendMail.mail';
+// import { mailTransporter } from 'src/mail/mail.provider';
 
 @Injectable()
 export class NotificationService {
     constructor(@Inject(forwardRef(() => UserService))
-    private readonly userService: UserService
+    private readonly userService: UserService,
+        private readonly mailProvider: MailProvider
     ) { }
 
     //send notification using nodemailer;
@@ -13,13 +15,12 @@ export class NotificationService {
 
         const user = this.userService.getUserById(userId);
 
-        await mailTransporter.sendMail({
+        await this.mailProvider.getTransporter().sendMail({
             from: '"My App" <no-reply@myapp.com>',
             to: user.email || 'vivek@test.com',
             subject: 'Notification',
             text: message,
         });
-
         Logger.log(`Send notification to ${user.name} : ${message}`)
 
     }
